fd0af994c4b060e53815ffe5199ae26dc486478a
dArKrEsQ <razvan.bielz@gmail.com>
2021-01-04 14:41:32 +0200

    finished app

--- App.js
+++ App.js
@@ -1,21 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import MovieList from './components/list';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import Detail from './components/detail';
+import Edit from './components/edit';
+import Auth from './components/auth';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const AppNavigator = createStackNavigator({
+  Auth: {screen: Auth},
+  MovieList: {screen: MovieList, navigationOptions: ({navigation}) => ({
+    headerShown: true,
+    title: "List of Movies",
+    headerStyle: {
+      backgroundColor: 'orange',
+    },
+    headerTintColor: '#fff',
+    headerTitleStyle: {
+      fontWeight: 'bold',
+      fontSize: 24,
+    }
+  })},
+  Detail: {screen: Detail, navigationOptions: ({navigation}) => ({
+    title: navigation.getParam('title'),
+    headerStyle: {
+      backgroundColor: 'orange',
+    },
+    headerTintColor: '#fff',
+    headerTitleStyle: {
+      fontWeight: 'bold',
+      fontSize: 24,
+    }
+  })},
+  Edit: {screen: Edit, navigationOptions: ({navigation}) => ({
+    headerTitleStyle: {
+      fontWeight: 'bold',
+      fontSize: 24,
+    }
+  })},
+},
+  {headerMode: 'screen'},
+)
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  headerButton: {
+    paddingRight: 5,
+  }
+})
+
+const App = createAppContainer(AppNavigator);
+
+export default App;
